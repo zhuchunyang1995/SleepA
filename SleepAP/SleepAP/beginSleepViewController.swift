671e985e5fe72c0bbba0f0ee70c0043b38db8627
sunxiayi <sunxiayi@umich.edu>
2018-10-19 20:10:45 -0400

    Finished the beginSleepVC.

--- SleepAP/SleepAP/beginSleepViewController.swift
+++ SleepAP/SleepAP/beginSleepViewController.swift
@@ -11,9 +11,14 @@ class beginSleepViewController: sleepRelatedViewController {
     
 
     @IBOutlet weak var sleepView: buttonView!
+    @IBOutlet weak var additionalDescription: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        additionalDescription.text = "😴 It’s time to go to sleep..."
+        additionalDescription.textAlignment = .center
+        additionalDescription.textColor = UIColor(red: 255, green: 255, blue: 255, alpha: 0.7)
+        additionalDescription.font = UIFont(name: "MontserratAlternates", size: 14)
     }
 }
