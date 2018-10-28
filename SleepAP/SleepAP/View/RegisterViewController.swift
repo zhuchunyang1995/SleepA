f2d3b3e4f2fd85d7c129fd668bd2fbac8e1d1c97
TYWu <wutian@umich.edu>
2018-10-28 15:47:53 -0400

    "signup and login done"

--- SleepAP/SleepAP/View/RegisterViewController.swift
+++ SleepAP/SleepAP/View/RegisterViewController.swift
@@ -7,13 +7,25 @@
 //
 
 import UIKit
+import Parse
 
 class RegisterViewController: UIViewController {
-
+    // outlets
+    @IBOutlet weak var usernmForReg: UITextField!
+    @IBOutlet weak var pswdForReg: UITextField!
+    @IBOutlet weak var pswdForComfirm: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        usernmForReg.resignFirstResponder()
+        pswdForReg.resignFirstResponder()
+        pswdForComfirm.resignFirstResponder()
     }
     
 
@@ -26,5 +38,40 @@ class RegisterViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    
+    
+    //actions
+    @IBAction func signUp(_ sender: Any) {
+        //check if username and password are nil
+        
+        if (usernmForReg.text == nil) || (pswdForReg.text == nil) || (pswdForComfirm.text == nil) {
+            //TODO
+            //do sth to notice the user to do it again
+            return
+        }
+        
+        let user = PFUser()
+        user.username = usernmForReg.text!
+        user.password = pswdForReg.text!
+        
+        if (pswdForComfirm.text! == pswdForReg.text!) {
+            user.signUpInBackground {
+                (succeeded, error) -> Void in
+                if (error == nil) && (succeeded == true){
+                    // sucessful!!
+                    let storyboard = UIStoryboard(name: "Login", bundle: nil)
+                    let vc = storyboard.instantiateViewController(withIdentifier: "loginViewController") as UIViewController
+                    self.present(vc, animated: true, completion: nil)
+                } else {
+                    // Show the errorString somewhere and let the user try again.
+                    let errorString = error!._userInfo?["error"] as? NSString
+                    //TODO: show the error srting to the user
+                    
+                }
+            }
+        }
+        
+        
+    }
 }
