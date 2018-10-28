0ec7d3bd24369c1014314f44ea255a32428d8272
zhuchuyang1995 <chunzhu@umich.edu>
2018-10-28 15:51:04 -0400

    editing summary

---
+++ SleepAP/SleepAP/View/summaryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  summaryViewController.swift
+//  SleepAP
+//
+//  Created by Chunyang Zhu on 10/28/18.
+//  Copyright © 2018 Wu, Tianyuan. All rights reserved.
+//
+
+import UIKit
+
+class summaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
