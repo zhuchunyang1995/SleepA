c7472c49a8ebe8954b540f7563608c914be00d47
TYWu <wutian@umich.edu>
2018-10-28 14:32:12 -0400

    login and signuo

---
+++ SleepAP/SleepAP/View/RegisterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterViewController.swift
+//  SleepAP
+//
+//  Created by tianyuan wu on 2018/10/27.
+//  Copyright © 2018 Wu, Tianyuan. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
