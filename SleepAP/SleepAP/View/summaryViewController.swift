c1f4eff1d2490078718f9bcaefa1f1d021e0ce24
zhuchuyang1995 <chunzhu@umich.edu>
2018-11-01 00:32:18 -0400

    basic graph

--- SleepAP/SleepAP/View/summaryViewController.swift
+++ SleepAP/SleepAP/View/summaryViewController.swift
@@ -7,13 +7,36 @@
 //
 
 import UIKit
+import Charts
+
 
 class summaryViewController: UIViewController {
 
+    @IBOutlet weak var lineChartView: LineChartView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        let Days = ["Mon","Tue","Wed","Thr","Fri","Sat","Sun"]
+        let Points = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0,17.0]
+        
+        setChart(dataPoints: Days, values: Points)
+    }
+    func setChart(dataPoints: [String], values: [Double]){
+        var dataEntries: [ChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = ChartDataEntry(x: Double(i), y: values[i])
+            dataEntries.append(dataEntry)
+        }
+        
+        let line1 = LineChartDataSet(values:dataEntries,label:"Points")
+        
+        let data = LineChartData()
+        data.addDataSet(line1)
+        lineChartView.data = data;
+        lineChartView.chartDescription?.text = "Summary"
     }
     
 
