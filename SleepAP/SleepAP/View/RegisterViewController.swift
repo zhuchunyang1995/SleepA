b77720594c7c20b251000840e6027b271ea423cf
TYWu <wutian@umich.edu>
2018-11-02 19:02:05 -0400

    login UI improved

--- SleepAP/SleepAP/View/RegisterViewController.swift
+++ SleepAP/SleepAP/View/RegisterViewController.swift
@@ -14,12 +14,19 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var usernmForReg: UITextField!
     @IBOutlet weak var pswdForReg: UITextField!
     @IBOutlet weak var pswdForComfirm: UITextField!
+    @IBOutlet weak var myView: UIView!
     
+    @IBOutlet weak var signUpBt: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let color1 = UIColor(rgb: 0x667eea)
+        let color2 = UIColor(rgb: 0x764ba2)
+        myView.setGradientBackground(colorOne: color1, colorTwo: color2)
+        signUpBt.backgroundColor = UIColor.white
+        signUpBt.layer.cornerRadius = 7
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -39,9 +46,17 @@ class RegisterViewController: UIViewController {
     }
     */
     
+    func showLogin () {
+        let storyboard = UIStoryboard(name: "Login", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "loginViewController") as UIViewController
+        self.present(vc, animated: true, completion: nil)
+    }
     
-    
-    //actions
+     //actions
+    @IBAction func goBack(_ sender: Any) {
+        self.showLogin()
+    }
+   
     @IBAction func signUp(_ sender: Any) {
         //check if username and password are nil
         
@@ -60,9 +75,8 @@ class RegisterViewController: UIViewController {
                 (succeeded, error) -> Void in
                 if (error == nil) && (succeeded == true){
                     // sucessful!!
-                    let storyboard = UIStoryboard(name: "Login", bundle: nil)
-                    let vc = storyboard.instantiateViewController(withIdentifier: "loginViewController") as UIViewController
-                    self.present(vc, animated: true, completion: nil)
+                    self.showLogin()
+                   
                 } else {
                     // Show the errorString somewhere and let the user try again.
                     let errorString = error!._userInfo?["error"] as? NSString
