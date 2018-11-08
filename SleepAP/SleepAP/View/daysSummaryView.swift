d4660a829907da4d04c7c4718a0ad916ab49ebf7
zhuchuyang1995 <chunzhu@umich.edu>
2018-11-08 11:14:43 -0500

    improve summary

---
+++ SleepAP/SleepAP/View/daysSummaryView.swift
@@ -0,0 +1,22 @@
+//
+//  daysSummaryView.swift
+//  SleepAP
+//
+//  Created by Chunyang Zhu on 11/5/18.
+//  Copyright © 2018 Wu, Tianyuan. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class daysSummaryView: LineChartView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- SleepAP/SleepAP/View/summaryViewController.swift
+++ SleepAP/SleepAP/View/summaryViewController.swift
@@ -10,24 +10,14 @@ import UIKit
 import Charts
 
 
-class summaryViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let myCell = UITableViewCell()
-        myCell.textLabel?.text = "Points"
-        myCell.detailTextLabel?.text = "8.0"
-        return myCell
-    }
-    
+class summaryViewController: UIViewController {
+
     
     let list = ["Your Score", "recommend "]
     
-    @IBOutlet weak var lineChartView: LineChartView!
+    @IBOutlet weak var weeksLineCharView: LineChartView!
     
-    @IBOutlet weak var TableView: UITableView!
+    @IBOutlet weak var lineChartView: LineChartView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,6 +30,7 @@ class summaryViewController: UIViewController,UITableViewDelegate,UITableViewDat
         let Points = [4.0, 4.0, 6.0, 3.0, 7.0, 6.0,5.0,7.0,4.0,9.0]
         
         setChart(dataPoints: Days, values: Points)
+        setWeeksChar(dataPoints: Days, values: Points)
     }
     func setChart(dataPoints: [String], values: [Double]){
         var dataEntries: [ChartDataEntry] = []
@@ -54,12 +45,39 @@ class summaryViewController: UIViewController,UITableViewDelegate,UITableViewDat
         let data = LineChartData()
         data.addDataSet(line1)
         lineChartView.data = data;
-        lineChartView.chartDescription?.text = "Summary"
+        lineChartView.chartDescription?.text = "Days"
         lineChartView.xAxis.labelPosition = XAxis.LabelPosition.bottom;
 //        lineChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: ["Points","  /Sleeping Hours"])
     }
+    func setWeeksChar(dataPoints:[String],values:[Double]){
+        var dataEntries:[ChartDataEntry] = []
+        for i in 0..<dataPoints.count {
+            let dataEntry = ChartDataEntry(x: Double(i), y: values[i])
+            dataEntries.append(dataEntry)
+        }
+        let line1 = LineChartDataSet(values:dataEntries,label:"Points")
+        let data = LineChartData()
+        data.addDataSet(line1)
+        weeksLineCharView.data = data
+        weeksLineCharView.chartDescription?.text = "Weeks"
+        weeksLineCharView.xAxis.labelPosition = XAxis.LabelPosition.bottom
+        weeksLineCharView.isHidden = true
+        
+        
+    }
     
-
+    @IBAction func daysButton(_ sender: UIButton) {
+        lineChartView.isHidden = false;
+        weeksLineCharView.isHidden = true;
+        
+    }
+    
+    @IBAction func weeksButton(_ sender: UIButton) {
+        lineChartView.isHidden = true;
+        weeksLineCharView.isHidden = false;
+       
+    }
+    
     /*
     // MARK: - Navigation
 
